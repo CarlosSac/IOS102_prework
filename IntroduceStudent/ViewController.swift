c3cb6ea5e5c3c6dfbaa62e3f20bc96547ee841bf
Carlos Sac <carlos.sac@outlook.com>
2025-08-05 19:06:54 -0400

    Add Logic for Outlets

--- IntroduceStudent/ViewController.swift
+++ IntroduceStudent/ViewController.swift
@@ -33,7 +33,18 @@ class ViewController: UIViewController {
         let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
 
 
-        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) cat(s). It is \(morePetsSwitch.isOn) that I want more pets."
+        let introduction = "Hello! My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) cat(s). It is \(morePetsSwitch.isOn) that I want more pets."
+        
+        // Creates the alert where we pass in our message, which our introduction.
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+
+        // A way to dismiss the box once it pops up
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+
+        // Passing this action to the alert controller so it can be dismissed
+        alertController.addAction(action)
+
+        present(alertController, animated: true, completion: nil)
 
         print(introduction)
     }
